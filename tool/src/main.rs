5fe2691c76c9d21290b1f6ed12c5d44cd5d23e21
Hanno Braun <hanno@braun-odw.eu>
2022-07-26 13:34:39 +0200

    Output all pull requests

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -2,19 +2,29 @@ use octocrab::params::{pulls::Sort, Direction, State};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let pull_requests = octocrab::instance()
-        .pulls("hannobraun", "Fornjot")
-        .list()
-        .state(State::Closed)
-        .sort(Sort::Created)
-        .direction(Direction::Ascending)
-        .per_page(100) // this is the maximum number of results per page
-        .page(0u32)
-        .send()
-        .await?;
+    let mut page = 1u32;
 
-    for pull_request in pull_requests.items {
-        println!("{}", pull_request.number);
+    loop {
+        let pull_requests = octocrab::instance()
+            .pulls("hannobraun", "Fornjot")
+            .list()
+            .state(State::Closed)
+            .sort(Sort::Created)
+            .direction(Direction::Ascending)
+            .per_page(100) // this is the maximum number of results per page
+            .page(page)
+            .send()
+            .await?;
+
+        for pull_request in pull_requests.items {
+            println!("{}", pull_request.number);
+        }
+
+        if pull_requests.next.is_some() {
+            page += 1;
+        } else {
+            break;
+        }
     }
 
     Ok(())
