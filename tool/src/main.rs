75f99aa6d1119fb8bc64f083a01f943341318884
Hanno Braun <hanno@braun-odw.eu>
2022-07-26 13:28:19 +0200

    Fetch pull requests

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,5 +1,19 @@
+use octocrab::params::{pulls::Sort, Direction, State};
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let pull_requests = octocrab::instance()
+        .pulls("hannobraun", "Fornjot")
+        .list()
+        .state(State::Closed)
+        .sort(Sort::Created)
+        .direction(Direction::Ascending)
+        .per_page(100) // this is the maximum number of results per page
+        .page(0u32)
+        .send()
+        .await?;
+
+    println!("{:?}", pull_requests);
+
     Ok(())
 }
