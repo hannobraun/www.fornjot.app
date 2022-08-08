09eabc4fc03efce0384fd375d9d5fb19a7119a0d
Hanno Braun <hanno@braun-odw.eu>
2022-08-08 13:17:43 +0200

    Create release announcement dir before file
    This causes an error otherwise.

--- tool/src/generate.rs
+++ tool/src/generate.rs
@@ -1,6 +1,8 @@
+use std::path::PathBuf;
+
 use anyhow::Context;
 use chrono::{Datelike, Utc};
-use tokio::fs::File;
+use tokio::fs::{self, File};
 
 pub async fn create_release_announcement() -> anyhow::Result<()> {
     let now = Utc::now();
@@ -8,10 +10,16 @@ pub async fn create_release_announcement() -> anyhow::Result<()> {
     let year = now.year();
     let week = now.iso_week().week();
 
-    let file = format!("content/blog/weekly-release/{year}-w{week}/index.md");
-    File::create(&file)
-        .await
-        .with_context(|| format!("Failed to create file `{file}`"))?;
+    let dir =
+        PathBuf::from(format!("content/blog/weekly-release/{year}-w{week}"));
+    let file = dir.join("index.md");
+
+    fs::create_dir_all(&dir).await.with_context(|| {
+        format!("Failed to create directory `{}`", dir.display())
+    })?;
+    File::create(&file).await.with_context(|| {
+        format!("Failed to create file `{}`", file.display())
+    })?;
 
     Ok(())
 }
