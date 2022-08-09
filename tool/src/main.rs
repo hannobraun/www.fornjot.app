f23fa64711011a0835b986b17d579474c56de7b1
Hanno Braun <hanno@braun-odw.eu>
2022-08-09 13:28:43 +0200

    Move pull request code to dedicated module

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,13 +1,13 @@
 mod args;
 mod generate;
-
-use std::collections::BTreeMap;
+mod pull_requests;
 
 use anyhow::Context;
-use chrono::{Date, Utc};
-use octocrab::params::{pulls::Sort, Direction, State};
 
-use self::{args::Args, generate::create_release_announcement};
+use self::{
+    args::Args, generate::create_release_announcement,
+    pull_requests::print_pull_requests_since_last_release,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -22,60 +22,6 @@ async fn main() -> anyhow::Result<()> {
                 .await
                 .context("Failed to create release announcement")?;
         }
-    }
-
-    Ok(())
-}
-
-async fn print_pull_requests_since_last_release(
-    last_release_date: Date<Utc>,
-) -> anyhow::Result<()> {
-    let mut pull_requests = BTreeMap::new();
-    let mut page = 1u32;
-
-    'outer: loop {
-        println!("Fetching page {}...", page);
-        let pull_request_page = octocrab::instance()
-            .pulls("hannobraun", "Fornjot")
-            .list()
-            .state(State::Closed)
-            .sort(Sort::Updated)
-            .direction(Direction::Descending)
-            .per_page(100) // this is the maximum number of results per page
-            .page(page)
-            .send()
-            .await?;
-
-        for pull_request in pull_request_page.items {
-            if let Some(updated_at) = pull_request.updated_at {
-                dbg!(updated_at);
-                if updated_at.date() < last_release_date {
-                    // This pull request has been updated before the last
-                    // release. Since we sort pull requests by
-                    // updated-descending, that means all following pull
-                    // requests have been updated before the last release, and
-                    // thus couldn't have been merged after.
-                    break 'outer;
-                }
-            }
-
-            if let Some(merged_at) = pull_request.merged_at {
-                if merged_at.date() >= last_release_date {
-                    pull_requests.insert(pull_request.number, pull_request);
-                }
-            }
-        }
-
-        if pull_request_page.next.is_some() {
-            page += 1;
-        } else {
-            break;
-        }
-    }
-
-    for (_, pull_request) in pull_requests {
-        let url = pull_request.html_url.expect("Pull request is missing URL");
-        println!("{}", url);
     }
 
     Ok(())
